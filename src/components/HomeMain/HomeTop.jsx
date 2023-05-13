f6df92acd589e3693a7d57cdcfb5578d6cccb252
Ilyos Xolmamtov <ilyosxolmamatov3@gmail.com>
2023-05-13 10:24:56 +0500

    home main section top add

--- src/components/HomeMain/HomeTop.jsx
+++ src/components/HomeMain/HomeTop.jsx
@@ -1,10 +1,45 @@
 import "./HomeMain.css";
+import "./kafedra.png";
+import mainImg from "./kafedra.png"
+
 
 
 export function HomeTop() {
+
     return (
-        <div>
-            HomeTop
+        <div className=" mainTop">
+            <div className="container">
+                <div className="mainImg">
+                    <img style={{ maxWidth: "100%", }} src={mainImg} alt="mainImg" />
+                    <p>
+                        Hozirda “Oliy matematika” kafedrasida katta pedagogik tajribaga, ilmiy salohiyatga ega bo‘lgan professor-o‘qituvchilardan: Professor: Abdullaev R.Z., Dotsentlar: R.R.Raxmatov, O.A. Islomova, Z.S. Chay,   K.A. Igamberdiyev, S.S.Sadaddinova, Mamatov A.E, Yaxshiboyev D, M.I.Oxunboyev, R.S.Xudazarov, N.Y.Jurayeva,  katta o‘qituvchilar:  Sh.E. Tadjibayeva, N.A. Raximova F.A, O.I. Abdug‘aniyeva, assistentlar: Sagdullayeva M.M Sh.M.Fayzullayev,   F.S.Abdullayeva, Nafasov A.Y, Raxmatova Z.A, Abdullayev O.K,  Eshmirzayev Sh.Sh,  Madatova Z.A, Sayfullayeva M.Z, Xurramov A.X kafedra mudiri dotsent O‘. Qalandarov rahbarligida ishlamoqdalar.
+                    </p>
+                </div>
+                <p>
+                    Oliy matematika kafedrasi birinchilar qatorida 1957 yilda tashkil etilgan. Kafedra 1955 yil tashkil etilgan oliy matematika va
+                    fizika kafedrasi asosida tashkil topgan. Oliy matematika kafedrasiga fizika-matematika fanlari nomzodi, dotsent ( 1964 yildan
+                    professor) M.K.Kamalov 1957 – 1972 yillar mudirlik qilgan. Oliy matematika kafedrasiga professor M.K.Kamolov vafotidan so‘ng,
+                    1973-1975 yillarda fizika-matematika fanlari nomzodi, dotsent M.J. Djurayev mudirlik qildi. Kafedraga fizika-matematika fanlari doktori, professor R.Ya. Suncheleyev (1975-1987 yillarda); fizika matematika fanlari nomzodi, dotsent E.I. Gegel (1987-1990 yillarda);   fizika matematika fanlari nomzodi, dotsent F.Sh.Majidova (1991-1993); dotsent B.B. Ergashev (1993-1998); dotsent R.N. Usmonov (1998-2003); O‘zFA akademigi, professor F.B. Abutaliyev (2003-2012); dotsent Y.M.Abduraxmanova (2012-2013); dotsent R.R.Raxmatov (2013-2021) mudirlik qildilar. 2021-2022 o‘quv yilidan boshlab Oliy matematika kafedrasiga f-m.f.n, dotsent O‘.N.Qalandarov kafedra mudiri bo‘lib faoliyat ko‘rsatmoqda.
+                </p>
+                <p>
+                    Oliy matematika kafedrasida turli davrlarda professor-o‘qituvchilar talabalarga chuqur bilim berib, ularning umumtexnik va mutaxassislik fanlarini yaxshi o‘zlashtirishlariga asos yaratdilar. Kafedrada uzoq yillar ishlagan, tajribali o‘qituvchilar qatorida katta o‘qituvchilar M.G.Gendler, X.Z. Baskina birinchi bo‘lib ish boshlaganlar.
+                </p>
+                <p>
+                    Keyingi yillarda dotsent A.I. Islomov, katta o‘qituvchilar A.M. Lebedev, V.A. Xolmskiy, assistent (keyinchalik fizika-matematika fanlari doktori, O‘zMU professori) Yo.R. Qo‘chqorov; dotsent M.D. Jo‘raev, katta o‘qituvchi R.Raimov, dotsentlar S.U. Krutyakov, E.S. Kozlova, A.U. Karimov, katta o‘qituvchi I. Validjonova, asistentlar R.Kamoliddinova, A.Zaytsev, katta o‘qituvchilar J.I. Mirxosilov, R.X. Xakimov, N.E. Alekseyeva, L.V.Frank, V.M. Obushnaya, T. K. Butko, asistentlar A.A. Agatayev, V.M. Gendler, dotsentlar SH.P. Primqulov, F.Buranov, katta o‘qituvchilar V.A.Jurayeva, I.L.Shopen, A.G.Xodjabagyan, asistentlar A.Sagintayev, M.M.Mamatovlar oliy matematika sirlarini talabalarga o‘rgatdilar.
+                </p>
+                <p>
+                    Oliy matematika kafedrasida tashkil etilgan vaqtdan boshlab, o‘quv-uslubiy va ilmiy tadqiqot ishlarini olib borishga katta ahamiyat berildi. Kafedra mudiri professor M.K. Kamolov “Tasodifiy miqdorlardan tuzilgan kvadratik fazolar taqsimoti va ularning tatbiqi” ga bag‘ishlangan mavzuda ilmiy ish olib bordi. U V.I.Privalovning “Analitik geometriya” darsligini o‘zbek tiliga tarjima qildi. Shuningdek kafedra o‘qituvchilari bilan hammualliflikda o‘quv-uslubiy qo‘llanmalarini yaratdi. Uning o‘zi o‘zbek tilida oliy o‘quv yurtlari uchun “Analitik geometriya” darsligini 1962 yilda nashrdan chiqardi.
+                </p>
+                <p>
+                    1998-2003 yillarda kafedraga tajribali o‘qituvchilar bilan birga yoshlar ham kelib qo‘shildilar. Bular: dotsentlar R.N.Usmonov, A.N.Mirzaev, A.E.Mamatov, R.R.Raxmatov, X.A.Abduvaitov, A.T. Temirbekov, katta o‘qituvchi X.M.Bulakbaev,  asistentlar: N.A. Pardaeva, X.D. Azizov, M.T. Payzieva, Sh.E. Tadjibayeva, O.I. Abdug‘aniyeva, F.S. Raximovalardir.
+                </p>
+                <p>
+                    1996 yilda Informatika kafedrasi tarkibidan “Ehtimollar nazariyasi va Statistika” kafedrasi ajralib chiqdi. Unga fizika-matematika fanlari doktori, professor Sh.A. Miraxmedov mudir etib saylandi. Kafedra yangi tashkil etilgan muhandislik - iqtisodiyot (hozirgi iqtisod va boshqaruv) fakultetiga biriktirildi. Kafedra ish faoliyatining boshlang‘ich davrida akademik Sh.Q. Farmonov, dotsentlar Yu.K.Axatov, Z.S.Chay, O.O. Norxojayev, asistentlar O.A. Saidova, O‘.N. Qalandarov  mehnat qilishdi. Kafedra faoliyati davomida Sh.A. Miraxmedov rahbarligida 2000 yil O‘.N.Qalandarov, 2001 yil O.A. Saidovalar (hozirda Islomova)  nomzodlik dissertatsiyalarini himoya qilishdi va universitet miqyosida barcha professor-o‘qituvchilari to‘laligicha ilmiy darajaga ega bo‘lgan yagona kafedraga aylanishdi. Kafedra faoliyati 2002 yil dekabrda tugatildi va “Oliy matematika” kafedrasiga qo‘shildi.
+                </p>
+                <p>
+                    Kafedra professor-o‘qituvchilari tomonidan 400 ga yaqin ilmiy  maqolalar, 200 dan ortiq uslubiy qo‘llanmalar, chop ettirilgan.   Akademik F.B. Abutaliyev rahbarligi ostida oliy matematika kafedrasi ilmiy yo‘nalishlaridan biri “Ekspert tizimlari asosida bilimlar bazasini qat’iymas mantiq usullari asosida qabul qilish tizimlar matematik modelini va dasturiy ta’minotini yaratish” ga bag‘ishlangan ilmiy-tadqiqot ishlari olib borildi.
+                </p>
+            </div>
         </div>
     )
 }
